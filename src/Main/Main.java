432be0d9fd77b371f4f087b38a1d17a97b27f1a1
Thomas Houlier <thoulier@enseirb-matmeca.fr>
2020-11-02 12:24:32 +0100

    Ask two clients at the beginning of the game

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -5,13 +5,19 @@ public class Main{
 
     public static void main(String[] args) {
 
-        System.out.println("Entrez le nom du joueur 1 : ");
 
-        Scanner clavier = new Scanner(System.in);
-        String nom_Joueur1 = clavier.next();
+        for (int i = 0; i < 2; i++) {
 
-        Joueur joueur1 = new Joueur(nom_Joueur1, "x");
-        System.out.println("Le nom du joueur 1 est : "+ joueur1.nom +"et sa forme est :" + joueur1.forme);
+            System.out.println("Entrez le nom du joueur" + i + " : ");
+
+
+            Scanner clavier = new Scanner(System.in);
+            String nom_Joueur = clavier.next();
+
+            Joueur joueur = new Joueur(nom_Joueur, "x");
+
+            System.out.println("Le nom du joueur"+ i + " est : " + joueur.nom + " et sa forme est :" + joueur.forme);
+        }
     }
 
 }
