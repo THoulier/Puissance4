30f242da1988975b80fc5c1b5ff2f5614441f3d0
GAUDICHAU Pierre-Malo <Pierre-Malo.Gaudichau@bordeaux-inp.fr>
2020-11-02 12:04:18 +0100

    Joueur

---
+++ src/Main/Joueur.java
@@ -0,0 +1,17 @@
+public class Joueur
+    public char nom;
+    public char forme;
+
+    public Joueur (char nom, char forme){
+        this.nom = nom;
+        this.forme = forme;
+    }
+
+    public getNom(){
+        return nom
+    }
+
+    public getForme(){
+        char forme = this.forme;
+        return forme
+    }
