973b5e66e6e6e6a7f7a7ecf6e7c9ca4e66f912aa
Thomas Houlier <thoulier@enseirb-matmeca.fr>
2020-11-02 12:17:00 +0100

    Amelioration

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -2,6 +2,7 @@
 import java.util.Scanner;
 
 public class Main{
+
     public static void main(String[] args) {
 
         System.out.println("Entrez le nom du joueur 1 : ");
@@ -10,7 +11,7 @@ public class Main{
         String nom_Joueur1 = clavier.next();
 
         Joueur joueur1 = new Joueur(nom_Joueur1, "x");
-        System.out.println("Le nom du joueur 1 est : "+ joueur1.nom);
+        System.out.println("Le nom du joueur 1 est : "+ joueur1.nom +"et sa forme est :" + joueur1.forme);
     }
 
 }
